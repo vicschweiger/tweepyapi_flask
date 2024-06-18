370cbc5189de701d99eff806328b787b102bc26b
vicschweiger <134645175+vicschweiger@users.noreply.github.com>
2024-06-18 13:15:32 -0300

    Adicionados templates de erro, header, footer, bem como seus respectivos arquivos css.

--- app.py
+++ app.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('index.html')
+
+@app.route('/error')
+def error():
+    return render_template('error.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
